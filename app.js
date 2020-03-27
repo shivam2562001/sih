541f2305013ac9f9d8aed3cc3240e79519851785
shivam2562001 <ssssss2562001@gmail.com>
2020-03-27 14:14:49 +0530

    deployed

--- app.js
+++ app.js
@@ -18,11 +18,13 @@ const User = require('./models/usermodel');
 
 dotenv.config({path : './config.env'});
 
-mongoose.connect(process.env.DATABASE_LOCAL, {
+mongoose.connect(process.env.DATABASE, {
     useNewUrlParser : true,
     useUnifiedTopology : true,
     useCreateIndex : true
-});
+}).then(con=>{
+    console.log("Database connected successfully")
+})
 
 //middleware for session
 app.use(session({
